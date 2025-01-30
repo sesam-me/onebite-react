563457e8f48bdcfbd9f2b64875588421e5bf0d85
seinlee <lsi6601@gmail.com>
2025-01-31 03:55:46 +0900

    투두리스트 프로젝트

--- section07/src/App.jsx
+++ section07/src/App.jsx
@@ -1,10 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
 import Viewer from "./components/Viewer";
 import Controller from "./components/Controller";
+import Even from "./components/Even";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+
+  const isMount = useRef(false);
+
+  // 1. 마운트: 탄생
+  useEffect(() => {
+    console.log("mount");
+  }, []);
+
+  // 2. 업데이트: 변화, 리렌더링
+  // deps를 생략하면, 리렌더링 될 때마다 실행된다.
+  // 만약, 처음에는 실행시키지 않고, 진짜 컴포넌트 업데이트 단계에서만 코드를 실행시키고 싶다면, 레퍼런스 객체를 하나 생성해서 플래그로서 사용하면 된다.
+  useEffect(() => {
+    if (!isMount.current) {
+      // false라면
+      isMount.current = true;
+      return;
+    }
+    console.log("update");
+  });
+
+  // 3. 언마운트 : 죽음
+  // 짝수일 때만 화면에 나오게 할것임
 
   const onClickButton = (value) => {
     setCount(count + value);
@@ -13,8 +37,15 @@ function App() {
   return (
     <div className="App">
       <h1>Simple Counter</h1>
+      <input
+        value={input}
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      />
       <section>
         <Viewer count={count} />
+        {count % 2 === 0 ? <Even /> : null}
       </section>
 
       <section>
